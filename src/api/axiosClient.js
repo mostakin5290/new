b62674ebe1fede453a1899bfb87bd89b30892b01
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-06-12 21:10:58 +0530

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosClient =  axios.create({
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://code-crack-1.onrender.com',
+    baseURL: 'https://code-crack.onrender.com',
     // baseURL: 'http://192.168.217.174:3000',
 
     withCredentials: true,
